5e9b71714838a16f6a6ba82eb556b224391f944d
Gilleschen <jhih_chen@tutk.com>
2017-03-17 17:06:03 +0800

    update2

---
+++ src/Vsaas/Login_Junit.java
@@ -0,0 +1,20 @@
+package Vsaas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import Vsaas.Login_TestCase;
+
+public class Login_Junit {
+	Login_TestCase testcase = new Login_TestCase();
+
+	@Test
+	public void test() {
+		assertTrue(testcase.Login_Z());
+	}
+
+}
---
+++ src/Vsaas/Login_TestCase.java
@@ -0,0 +1,58 @@
+package Vsaas;
+
+import java.awt.AWTException;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import Vsaas.Platform;
+import Vsaas.Platform;
+
+public class Login_TestCase {
+
+	Platform web = new Platform();
+	int sec = 10;
+	WebDriverWait[] wait = new WebDriverWait[web.driver.length];
+
+	public Login_TestCase() {
+		for (int i = 0; i < web.driver.length; i++) {
+			wait[i] = new WebDriverWait(web.driver[i], sec);
+		}
+
+	}
+
+	public boolean Login_Z() {
+
+		boolean result = true;
+		String methodname = Thread.currentThread().getStackTrace()[1].getMethodName();
+		for (int i = 0; i < web.driver.length; i++) {
+			Wait_Element("email", methodname, i);
+			web.driver[i].findElement(By.name("email")).sendKeys("jhih_chen@sina.com");
+			web.driver[i].findElement(By.name("password")).sendKeys("888888ii");
+			web.driver[i].findElement(By.className("btn")).click();
+		}
+		for (int i = 0; i < web.driver.length; i++) {
+			QuitDriver(i);
+		}
+
+		return result;
+	}
+
+	void Wait_Element(String element, String methodname, int i) {
+
+		try {
+			wait[i].until(ExpectedConditions.presenceOfElementLocated(By.name(element)));
+		} catch (Exception ex) {
+
+			System.out.println("MethodName:" + this.getClass().getName() + "." + methodname);
+			System.out.println("Not found " + element);
+		}
+
+	}
+
+	void QuitDriver(int i) {
+		web.driver[i].quit();
+	}
+}
